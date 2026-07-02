180d5a137292126be916e892dad5ac5ff9f03f23
JaydenWongZhen <taymeister@gmail.com>
2026-07-02 10:39:41 +0000

    coomit practise

--- L01_redCircle.js
+++ L01_redCircle.js
@@ -1,9 +1,18 @@
 function setup(){
-    createCanvas(600, 400);
+    createCanvas(600, 600);
     background(220);
 }
 
 function draw(){
+    noStroke();    
+    fill(150,150,150);
+    rect(250,100,100,300);    
     fill(255, 0, 0);
-    ellipse(300,200,200,200);
+    ellipse(300,150,75,75);
+    fill(255,225,0);
+    ellipse(300,250,75,75);
+    fill(100,255,100);
+    ellipse(300,350,75,75);
+    fill(150,150,150);
+    rect(275,400,50,250);
 }
