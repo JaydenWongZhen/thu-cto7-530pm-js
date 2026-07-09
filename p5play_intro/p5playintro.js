aee6755171add01f723c89a91410b29fd4958789
JaydenWongZhen <taymeister@gmail.com>
2026-07-09 10:19:25 +0000

    Jul 9, 2026, 10:19 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -11,8 +11,8 @@ function setup() {
  ball.y = 50;
  ball.diameter = 80;
  ball.color = "magenta";
- 
- squar = new Sprite(200,100,100,50);
+
+ squar = new Sprite();
  squar.color = "blue";
   // End Basic shape testing
 
