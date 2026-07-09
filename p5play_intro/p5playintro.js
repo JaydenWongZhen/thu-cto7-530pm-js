e2a300f922503ac691cc347a6914f659ccf56337
JaydenWongZhen <taymeister@gmail.com>
2026-07-09 10:34:43 +0000

    Jul 9, 2026, 10:34 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -27,7 +27,7 @@ function draw() {
   background(250);
   textSize(16);
   text('ball:'+ round(ball.x) + "," + round(ball.y),20,50);
-  if (ball.y > 600 || ball.y < 0 || ball.x < 375 || ball.x > 0) {
+  if (ball.y > 600 || ball.y < 0 || ball.x < 400 || ball.x > 0) {
     ball.vel.y = ball.vel.y * -1
   }
 }
