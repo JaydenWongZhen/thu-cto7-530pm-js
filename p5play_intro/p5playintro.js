3839e233f546e6d00abf3dc7d8d3545447aee8a1
JaydenWongZhen <taymeister@gmail.com>
2026-07-09 10:42:42 +0000

    Jul 9, 2026, 10:42 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -26,7 +26,7 @@ function draw() {
   // write your codes here
   background(250);
   textSize(25);
-  text("happi birthday mom",200,50);
+  text("happi birthday mom",150,50);
   if (ball.y > 600 || ball.y < 0) {
     ball.vel.y = ball.vel.y * -1;
   }
