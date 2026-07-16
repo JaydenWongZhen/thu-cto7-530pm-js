1e9f03e1a50a73a49689338042a345be065945c8
JaydenWongZhen <taymeister@gmail.com>
2026-07-16 10:23:32 +0000

    Jul 16, 2026, 10:23 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -12,7 +12,7 @@ function setup() {
   // --- Exercise: Sum of first 10 even numbers ---
   // write your codes here
   for(let i = 1; i<=20; i++) {
-    console.log(i)
+    text()
   }
   // --- Exercise: Age category classification ---
   // write your codes here
