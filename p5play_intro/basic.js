941dadd0d218f24ece3da3225dbeef880ddfdc6c
JaydenWongZhen <taymeister@gmail.com>
2026-07-16 10:53:24 +0000

    Jul 16, 2026, 10:53 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -12,7 +12,7 @@ function setup() {
   // --- Exercise: Sum of first 10 even numbers ---
   // write your codes here
   let dih = 0;
-  for(let i = 0; i<=10; i+=2) {
+  for(let i = 0; i<=20; i+=2) {
     dih = dih + i;
   }
   text(dih,50,50);
