4bd4fc1c9f520cc3279c083dfa474d43c9c899e7
JaydenWongZhen <taymeister@gmail.com>
2026-07-16 11:00:01 +0000

    Jul 16, 2026, 11:00 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -18,7 +18,7 @@ function setup() {
   text(dih,50,50);
   // --- Exercise: Age category classification ---
   // write your codes here
-  let age = 10;
+  let age = 16;
 if (age >0 && age <2){
   console.log("conifmed age: infant");
 }
