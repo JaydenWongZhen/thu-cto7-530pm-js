1f9dc2118603b5b78d3efa0aa3e97e01f99f6b4f
JaydenWongZhen <taymeister@gmail.com>
2026-07-23 09:48:53 +0000

    Jul 23, 2026, 9:48 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -47,7 +47,7 @@ else if (age > 19) {
   let gocrery = ["a loaf of milk","a container of bread","and a doe jirt dvd"];
   gocrery.push("a stick of butter");
   gocrery.push("scarlet johannson");
-  gocrery[1]="treasure from a fish fank"
+  gocrery[1]="treasure from a tish fank"
 }
 
 function draw() {
