7192323b46a98891e9a2539c186770708e7e9b81
JaydenWongZhen <taymeister@gmail.com>
2026-07-23 10:55:10 +0000

    Jul 23, 2026, 10:55 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -9,7 +9,7 @@ function preload() {
 function setup() {
     new Canvas(400,600);
     background(255);
-    birt = new Sprite();
+    bird = new Sprite();
     bird.x = width/2;
     bird.y = 200;
     bird.width = 34;
