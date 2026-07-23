f0e8f11b8522b33608983c2c7749cbfa853330a6
JaydenWongZhen <taymeister@gmail.com>
2026-07-23 09:35:08 +0000

    Jul 23, 2026, 9:35 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -36,7 +36,7 @@ else if (age > 19) {
 }
   // --- Exercise: Display odd numbers backward using while loop ---
   // write your codes here
-
+  let peepee = 
   // --- Exercise: Array operations (groceries) ---
   // write your codes here
 }
