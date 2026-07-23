be3ebd2c677e63c1d58ffb906fa8f73d82b4d130
JaydenWongZhen <taymeister@gmail.com>
2026-07-23 10:42:55 +0000

    Jul 23, 2026, 10:42 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -2,7 +2,7 @@
 let birt, floor;
 let midairbirtflarp, background, base;
 function preload() {
-    midairbirt = loadImage("assets/")
+    midairbirtflarp = loadImage("assets/bluebird")
 }
 function setup() {
     new Canvas(400,600);
