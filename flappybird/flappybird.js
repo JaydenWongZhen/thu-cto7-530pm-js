02c12711327e17a0818e6674b18f93640a5be35f
JaydenWongZhen <taymeister@gmail.com>
2026-07-23 10:49:36 +0000

    Jul 23, 2026, 10:49 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -12,5 +12,5 @@ function setup() {
 }
 function draw() {
     image(background,0,0,width,height);
-    image(base,0,height-height/4,width,height/4);
+    // image(base,0,height-height/4,width,height/4);
 }
