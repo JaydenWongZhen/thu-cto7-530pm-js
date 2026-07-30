404c7650f473ffd100f6ab8741b0121f0f2d87be
JaydenWongZhen <taymeister@gmail.com>
2026-07-30 09:52:31 +0000

    Jul 30, 2026, 9:52 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -20,7 +20,7 @@ function setup() {
     world.gravity.y = 1;
     floor = new Sprite();
     floor.x = 200;
-    floor.y= height-height/4;
+    floor.y= height-20;
     floor.width = 400;
     floor.height = 125;
     floor.collider = "static";
