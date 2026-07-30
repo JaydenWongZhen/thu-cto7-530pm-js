6d3a0687e6fa13100fbaf560d440b9b7123d6293
JaydenWongZhen <taymeister@gmail.com>
2026-07-30 10:06:32 +0000

    Jul 30, 2026, 10:06 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -31,7 +31,7 @@ function draw() {
     image(backg,0,0,width,height);
     fill("red");
     textSize(15);
-    text("bird_sleep: ")
+    text("bird_sleep: " + bird.sleeping )
 
     if (kb.presses('space')) {
         bird.sleeping = false;
