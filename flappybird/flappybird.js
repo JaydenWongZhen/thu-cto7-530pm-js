d6c8aa4a1e250346aed9763e119a787b6d4da657
JaydenWongZhen <taymeister@gmail.com>
2026-07-30 09:38:08 +0000

    Jul 30, 2026, 9:38 AM

---
+++ flappybird/flappybird.js
@@ -0,0 +1,24 @@
+// the final game
+let bird, floor;
+let midairbirtflarp, backg, base;
+function preload() {
+    midairbirtflarp = loadImage("assets/bluebird-midflap.png");
+    backg = loadImage("assets/background-day.png");
+    base = loadImage("assets/base.png")
+}
+function setup() {
+    new Canvas(400,600);
+    background(255);
+    bird = new Sprite();
+    bird.x = width/2;
+    bird.y = 200;
+    bird.width = 34;
+    bird.height = 24;
+    bird.img = midairbirtflarp;
+    bird.collider = "dynamic";
+    bird.bounciness = 0.77;
+}
+function draw() {
+    image(backg,0,0,width,height);
+    image(base,0,height-height/4,width,height/4);
+}
