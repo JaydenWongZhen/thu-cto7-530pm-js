0cce3eb805b5e9259f32d2cd4df9c64f699a79e2
JaydenWongZhen <taymeister@gmail.com>
2026-07-30 10:47:52 +0000

    Jul 30, 2026, 10:47 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -71,5 +71,5 @@ if (frameCount === 1) {
 function spawnPipePair() {
     let gap = 50;
     let midY=height/2;
-    bottompipe = new Sprite(400,someY)
+    bottompipe = new Sprite(400,someY,52,320,"static")
 }
