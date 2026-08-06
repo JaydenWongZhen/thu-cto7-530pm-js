bd424bf9dd3b4764d251c775274c1617f4f3d995
JaydenWongZhen <taymeister@gmail.com>
2026-08-06 09:36:55 +0000

    Aug 6, 2026, 9:36 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -77,6 +77,7 @@ function spawnPipePair() {
     pipegroup.add(bottompipe);
     pipegroup.layer = 0;
     
+    randY
     toppipe = new Sprite(400,height-randY,52,320,"static");
     toppipe.img = pipeimg;
     toppipe.rotation=180;
