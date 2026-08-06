695bf9bfa2eac116d3d7ac3816ca2bf86115d953
JaydenWongZhen <taymeister@gmail.com>
2026-08-06 10:39:20 +0000

    Aug 6, 2026, 10:39 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -5,7 +5,7 @@ let upbirdflap,downbirdflap;
 let pipegroup;
 let pipeimg;
 let bottompipe, toppipe;
-let Ghdjfgdhktrnhvkbtv
+let gameover;
 function preload() {
     midairbirdflap = loadImage("assets/bluebird-midflap.png");
     backg = loadImage("assets/background-day.png");
