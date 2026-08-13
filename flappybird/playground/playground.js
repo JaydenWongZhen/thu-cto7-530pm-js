0c940a3c1640fccdaed4545b068b944c1c6ffe73
JaydenWongZhen <taymeister@gmail.com>
2026-08-13 10:48:57 +0000

    Aug 13, 2026, 10:48 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -26,5 +26,5 @@ function draw() {
      fill("red");
      text("birb.sleep: " + birb.sleep , 20,40);
      text('birb.vel.y: ' + birb.vel.y , 20,60);
-    text('from countr: ' + frame)
+    text('from countr: ' + frameCount, 20,80);
 }
