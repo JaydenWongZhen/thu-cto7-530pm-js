dab88a2571bf70516b619e1367dc50c82f473e6b
JaydenWongZhen <taymeister@gmail.com>
2026-08-13 10:25:54 +0000

    Aug 13, 2026, 10:25 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -8,7 +8,7 @@ function preload() {
 function setup() {
     new Canvas(400,600);
     background("lightblue");
-    birb = new Sprite(width/2,height/2,width/2,height/2,"dynamic");
+    birb = new Sprite(width/2,height/2,25,height/2,"dynamic");
     birb.img=flapmid;
     floor = new Sprite(width,height/3,0,height-height/3,"static");
     floor.img = base;
