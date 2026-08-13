5e90d79c1f74c22928dacebf600041e6fd899ecb
JaydenWongZhen <taymeister@gmail.com>
2026-08-13 09:43:58 +0000

    Aug 13, 2026, 9:43 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -37,7 +37,7 @@ function setup() {
     floor.img = base;
     // bird.vel.x = 1;
     pipegroup = new Group();
-    startmessageimg = new Sprite()
+    startmessageimg = new Sprite(0,0,)
 }
 function draw() {
     image(backg,0,0,width,height);
