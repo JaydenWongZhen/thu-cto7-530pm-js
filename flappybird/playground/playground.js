7798de540216ae97ac80f7a19b34ce9d7326a204
JaydenWongZhen <taymeister@gmail.com>
2026-08-13 10:19:17 +0000

    Aug 13, 2026, 10:19 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -8,7 +8,7 @@ function preload() {
 function setup() {
     new Canvas(500,700);
     background(255);
-    birb = new SpeechRecognitionErrorEvent()
+    birb = new Sprite()
 }
 function draw() {
 
