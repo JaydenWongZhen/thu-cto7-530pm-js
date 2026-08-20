2d689aaef4e468299e6338f057d46e51be91f9f9
JaydenWongZhen <taymeister@gmail.com>
2026-08-20 10:05:42 +0000

    Aug 20, 2026, 10:05 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -3,7 +3,10 @@ let score = 0;
 function preload() {
     let prefix = "assets/";
     let suffix = ".png";
-    let filename = ""
+    let filename = "";
+    for (let i=0; i <= 10; i++) {
+
+    }
 }
 function setup() {
     new Canvas(400,600);
