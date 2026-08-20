dffe18da00a375be352b27a79053b5fc68d10946
JaydenWongZhen <taymeister@gmail.com>
2026-08-20 10:38:06 +0000

    Aug 20, 2026, 10:38 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -29,7 +29,7 @@ function draw() {
     if (kb.presses("down")) {
         score = score -1;
     }
-    score = constrain(score,0,Infinity);
+    score = constrain(score,0,);
     textSize = 15;
     fill("blue");
     text("score: " + score,20,20);
