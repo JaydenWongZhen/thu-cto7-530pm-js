5f3711175cecc0904b54eec2c48c09dca6fbcdd8
JaydenWongZhen <taymeister@gmail.com>
2026-08-27 10:05:05 +0000

    Aug 27, 2026, 10:05 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -73,6 +73,6 @@ function moveGroup() {
     let count = scoreGroup.length;
     let fullwidth = count * 25;
     let centerScoreGroup = fullwidth/2;
-    let cet
+    let centerSingle = 25/2;
     let difference = centerScoreGroup - centerSingle;
 }
