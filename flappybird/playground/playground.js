03292604a3f1b30bb66819ebfd20088b6b28c0ed
JaydenWongZhen <taymeister@gmail.com>
2026-08-27 09:50:43 +0000

    Aug 27, 2026, 9:50 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -31,6 +31,9 @@ function draw() {
     if (kb.presses("down")) {
         score = score -1;
     }
+        if (kb.presses("3")) {
+        score = round(random(100,999));
+    }
     if (kb.presses("3")) {
         score = round(random(100,999));
     }
