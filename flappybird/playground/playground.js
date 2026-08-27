bdc27927e860af87619563fa7a95bb5a0740ff76
JaydenWongZhen <taymeister@gmail.com>
2026-08-27 09:59:55 +0000

    Aug 27, 2026, 9:59 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -54,7 +54,7 @@ function drawScore() {
     // if (score > 10) {
     //     noLoop();
     // }
-    let middle = camera.x - offset;
+    let middle = camera.x;
     let offset = 0;
     for (let one of digitvararray) {
         let num = new Sprite();
