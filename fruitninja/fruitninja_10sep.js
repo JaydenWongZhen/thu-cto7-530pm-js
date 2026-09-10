3c81da09ade3ebf642e6c0ccda3515bda7a97f33
JaydenWongZhen <taymeister@gmail.com>
2026-09-10 10:46:42 +0000

    Sep 10, 2026, 10:46 AM

--- fruitninja/fruitninja_10sep.js
+++ fruitninja/fruitninja_10sep.js
@@ -5,7 +5,7 @@ function preload() {
     dojobg=loadImage("assets/dojobackground.png");
 }
 function setup() {
-    createCanvas(700,600);
+    createCanvas(800,600);
     background(255);
     world.gravity.y=10;
 }
