4e37a25c968f3f2f7844c36375c5ca6ea12df9b2
JaydenWongZhen <taymeister@gmail.com>
2026-09-10 10:53:14 +0000

    Sep 10, 2026, 10:53 AM

--- fruitninja/fruitninja_10sep.js
+++ fruitninja/fruitninja_10sep.js
@@ -6,7 +6,7 @@ let fruitvariety=[];
 function preload() {
     dojobg=loadImage("assets/dojobackground.png");
     let peach = {
-        whole: loadImage()
+        whole: loadImage(assets/peachwhole)
     }
 }
 function setup() {
