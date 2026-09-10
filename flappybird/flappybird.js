8a30fc5fe11a1debab44d098b5e8440be7c74b61
JaydenWongZhen <taymeister@gmail.com>
2026-09-10 09:58:00 +0000

    Sep 10, 2026, 9:58 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -132,6 +132,7 @@ function draw() {
             bird.rotation= 0;
             bird.collider = "static";
             bird.y=200;
+            bird.visible= "false"
 
             gameover.remove();
             startmessage.visible= "true";
