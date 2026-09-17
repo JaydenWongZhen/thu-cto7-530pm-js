3f63460441e88e6048ca9bc7f3360c66cfdf5f31
JaydenWongZhen <taymeister@gmail.com>
2026-09-17 10:25:36 +0000

    Sep 17, 2026, 10:25 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -32,7 +32,7 @@ function draw() {
 }
 }
 function spawnFruit() {
-    fruit = new Sprite(width);
+    fruit = new Sprite(width/2,400);
     let fruitvariation=random(fruits);
     fruit.diameter=35;
     fruit.img=fruitvariation.whole;
