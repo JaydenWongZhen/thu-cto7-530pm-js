36df8f2d6869160ffd3e5844197a2ef413175431
JaydenWongZhen <taymeister@gmail.com>
2026-09-17 10:11:56 +0000

    Sep 17, 2026, 10:11 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -7,7 +7,7 @@ function preload() {
     dojobg=loadImage("assets/dojobackground.png");
     let peach = {
         whole: loadImage("assets/peachwhole.png"),
-        half: loadImage("assets/.png")
+        half: loadImage("assets/peachhalf.png")
     };
     let melon = {
         whole: loadImage("assets/watermelonwhole.png"),
