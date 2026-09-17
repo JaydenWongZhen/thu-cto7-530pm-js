c26d989d85ac2c83f66710b86e9fa9a617f353b0
JaydenWongZhen <taymeister@gmail.com>
2026-09-17 10:06:10 +0000

    Sep 17, 2026, 10:06 AM

--- fruitninja/playground.js
+++ fruitninja/playground.js
@@ -8,7 +8,7 @@ citship:"mango"
 let adress = {
     strret: "nihgue raezih",
     blknum:"stupid 69 692-b",
-    unitnum: "#19248294-"
+    unitnum: "#1924829"
 }
 function setup() {
 print(person);
