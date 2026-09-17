2d63ffb196b4afb414e1e320e7b3fe46b3ab92e0
JaydenWongZhen <taymeister@gmail.com>
2026-09-17 10:01:14 +0000

    Sep 17, 2026, 10:01 AM

--- fruitninja/playground.js
+++ fruitninja/playground.js
@@ -1,11 +1,11 @@
 let person = {
-    
+born = "hungary",
+gender = true,
+race = "crossiant",
+bankacc = -777,
+citship = "mango"
 };
-person.born = "hungary";
-person.gender = true;
-person.race = "crossiant";
-person.bankacc = -777;
-person.citship = "mango";
+
 function setup() {
 print(person);
 }
