ebcc55a9cc26a10dd4882a5e54b0d6d9eed26409
JaydenWongZhen <taymeister@gmail.com>
2026-09-17 10:54:46 +0000

    Sep 17, 2026, 10:54 AM

---
+++ fruitninja/fruitninja.js
@@ -0,0 +1,58 @@
+// write your codes here
+let dojobg;
+let fruitgrop;
+let fruits=[];
+let mydebug = true;
+function preload() {
+    dojobg=loadImage("assets/dojobackground.png");
+    let peach = {
+        whole: loadImage("assets/peachwhole.png"),
+        Lhalf: loadImage("assets/peachhalf.png"),
+        Rhalf: loadImage("assets/peachhalf2.png"),
+        half: loadImage("assets/peachsplash.png")
+    };
+    let melon = {
+        whole: loadImage("assets/watermelonwhole.png"),
+        Lhalf: loadImage("assets/watermelonhalf.png"),
+        Rhalf: loadImage("assets/watermelonhalf.png"),
+        half: loadImage("assets/watermelonsplash.png")
+    };
+    fruits=[peach,melon];
+}
+function setup() {
+    createCanvas(800,400);
+    background(255);
+    world.gravity.y=10;
+    fruitgrop = new Group();
+}
+function draw() {
+    image(dojobg,0,0,width,height);
+    if (frameCount%120 === 0) {
+    spawnFruit();
+    }
+    if (kb.presses("1")) {
+    
+        mydebug=!mydebug;
+        
+    }
+    if (mouse.pressing()) {
+        noStroke();
+        let swoontrail = new Sprite(mouseX,mouseY,10);
+        swoontrail.color="#FFFFFF";
+        swoontrail.stroke="#FFFFFF"
+        swoontrail.collider="none";
+        swoontrail.life=20;
+    }
+}
+function spawnFruit() {
+    fruit = new Sprite(random(200,600),400);
+    let fruitvariation=random(fruits);
+    fruit.diameter=35;
+    fruit.vel.y=-10;
+    fruit.vel.x=random(-5,5);
+    fruit.img=fruitvariation.whole;
+    fruitgrop.add(fruit);
+    fruit.type=fruitvariation;
+    fruit.friction=2;
+    fruit.debug = mydebug;
+}
