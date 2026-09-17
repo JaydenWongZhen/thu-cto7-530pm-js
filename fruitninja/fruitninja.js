3999ee07b4162968e83ccd28d2a72cb01256e8d7
JaydenWongZhen <taymeister@gmail.com>
2026-09-17 10:13:41 +0000

    Sep 17, 2026, 10:13 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -8,12 +8,12 @@ function preload() {
     let peach = {
         whole: loadImage("assets/peachwhole.png"),
         Lhalf: loadImage("assets/peachhalf.png"),
-        Rhalf: loadImage("assets/peachhalf2.png")
+        Rhalf: loadImage("assets/peachhalf2.png"),
     };
     let melon = {
         whole: loadImage("assets/watermelonwhole.png"),
         Lhalf: loadImage("assets/watermelonhalf.png"),
-        Rhalf: loadImage("assets/watermelonhalf2.png")
+        Rhalf: loadImage("assets/watermelonhalf2.png"),
     };
     fruits=[peach,melon];
 }
