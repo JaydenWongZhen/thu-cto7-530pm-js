12ea959613ea1881b25c738a6883430704487095
JaydenWongZhen <taymeister@gmail.com>
2026-09-17 10:35:52 +0000

    Sep 17, 2026, 10:35 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -31,7 +31,7 @@ function draw() {
     spawnFruit();
     }
     if (kb.presses("1")) {
-        fruit.debug=!fruit.debug;
+        fruitgrop.debug=!fruitgrop.debug;
     }
 }
 function spawnFruit() {
